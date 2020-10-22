8fc5029d68fc6829de7aee5e9fba4ade79498884
mgrzwcz <55543943+mgrzwcz@users.noreply.github.com>
2020-10-22 23:15:31 +0100

    Fourth

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -8,7 +8,8 @@ const connectDatabase = async () => {
     await mongoose.connect(database, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useFindAndModify: false
     });
     console.log("Mongo connected");
   } catch (err) {
--- routes/api/user/profile.js
+++ routes/api/user/profile.js
@@ -1,9 +1,120 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../../middlewares/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../../../models/User");
+const Profile = require("../../../models/Profile");
 
-//route:        GET api/user/profile
-//desc:         Profile per user
+//route:        GET api/user/profile/me
+//desc:         Get current user's profile
 //access:       Private
-router.get('/', (req,res) => res.send('User profile route'));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//route:        POST api/user/profile
+//desc:         Create or Update user profile
+//access:       Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    //Validation
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //Build profile object
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    //Build social object
+
+    profileFields.social = {};
+
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+
+    //Inserting the data
+
+    try {
+      //when using a Mongoose method after async, we need to use await in front of the method
+
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        //Update if found
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+            //and return the entire Profile JSON
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
